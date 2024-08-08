3aa07011b4d338797e8afb62118d98a1c700d8dd
hareesh1532 <hareeshmecch@gmail.com>
2024-08-08 15:45:39 +0530

    1st commit

---
+++ src/test/java/practice/PracticeGit.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class PracticeGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/practice/PracticeGitHub.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class PracticeGitHub {
+
+	public static void main(String[] args) {
+		System.out.println("Start");
+	
+
+	}
+
+}
